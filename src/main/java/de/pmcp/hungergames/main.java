252c03a732b8505e82e075692769d2bf231053c0
BuildTools <unconfigured@null.spigotmc.org>
2022-09-08 20:50:36 +0200

    freeze aber aufgegeben

--- src/main/java/de/pmcp/hungergames/main.java
+++ src/main/java/de/pmcp/hungergames/main.java
@@ -1,14 +1,21 @@
 package de.pmcp.hungergames;
 
+import de.pmcp.hungergames.pregame.Freeze;
+import de.pmcp.hungergames.pregame.isfreeze;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class main extends JavaPlugin {
-boolean freeze = false;
     @Override
     public void onEnable() {
         // Plugin startup logic
         Bukkit.getLogger().info("PMCP Hungergames ist nun gestartet.");
+        //cmd register
+        this.getCommand("freeze").setExecutor(new isfreeze());
+        //listener register
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        pluginManager.registerEvents(new Freeze(), this);
     }
 
     @Override
--- src/main/java/de/pmcp/hungergames/pregame/Freeze.java
+++ src/main/java/de/pmcp/hungergames/pregame/Freeze.java
@@ -1,18 +1,18 @@
 package de.pmcp.hungergames.pregame;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import static de.pmcp.hungergames.pregame.isfreeze.isfreeze;
+
 
 public class Freeze implements Listener {
     public void onmove(PlayerMoveEvent e) {
-
+        Player player = e.getPlayer();
         if (de.pmcp.hungergames.pregame.isfreeze.isfreeze == true) {
             e.setCancelled(true);
-
+            player.sendMessage("Du darfst dich nicht bewegen!");
         }
+        else {e.setCancelled(false);}
     }
 }

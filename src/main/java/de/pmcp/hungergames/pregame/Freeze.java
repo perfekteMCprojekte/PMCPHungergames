89a2aed21fedb8179daf5e0a0022f165a058af27
BuildTools <unconfigured@null.spigotmc.org>
2022-09-08 20:20:53 +0200

    freeze aber aufgegeben

--- src/main/java/de/pmcp/hungergames/pregame/Freeze.java
+++ src/main/java/de/pmcp/hungergames/pregame/Freeze.java
@@ -5,11 +5,12 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import static de.pmcp.hungergames.pregame.isfreeze.isfreeze;
 
 public class Freeze implements Listener {
     public void onmove(PlayerMoveEvent e) {
 
-        if (de.pmcp.hungergames.pregame.isfreeze == true) {
+        if (de.pmcp.hungergames.pregame.isfreeze.isfreeze == true) {
             e.setCancelled(true);
 
         }

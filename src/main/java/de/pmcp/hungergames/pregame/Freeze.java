d88aed2b41083a2260d4a9d668e2bca2b9c0f0bf
BuildTools <unconfigured@null.spigotmc.org>
2022-09-09 22:20:19 +0200

    + Freeze

--- src/main/java/de/pmcp/hungergames/pregame/Freeze.java
+++ src/main/java/de/pmcp/hungergames/pregame/Freeze.java
@@ -1,12 +1,16 @@
 package de.pmcp.hungergames.pregame;
 
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import static de.pmcp.hungergames.pregame.isfreeze.isfreeze;
 
 
 public class Freeze implements Listener {
+    @EventHandler(ignoreCancelled = true)
     public void onmove(PlayerMoveEvent e) {
         Player player = e.getPlayer();
         if (de.pmcp.hungergames.pregame.isfreeze.isfreeze == true) {

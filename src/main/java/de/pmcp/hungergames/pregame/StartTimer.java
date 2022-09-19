1622452cd8cdecee6d95f3592f9b18e0ba6bc4cc
BuildTools <unconfigured@null.spigotmc.org>
2022-09-19 22:09:01 +0200

    Nochmal Leerzeichen gesetzt

--- src/main/java/de/pmcp/hungergames/pregame/StartTimer.java
+++ src/main/java/de/pmcp/hungergames/pregame/StartTimer.java
@@ -22,46 +22,46 @@ public class StartTimer implements CommandExecutor {
                 //Timer
                 switch (sec) {
                     case 600:
-                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP] " + ChatColor.AQUA + "Die Hungergames starten in " + ChatColor.RED + "10 " + ChatColor.AQUA + "Minuten");
+                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP]" + ChatColor.AQUA + " Die Hungergames starten in" + ChatColor.RED + " 10" + ChatColor.AQUA + " Minuten");
                         break;
                     case 300:
-                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP] " + ChatColor.AQUA + "Die Hungergames starten in " + ChatColor.RED + "5 " + ChatColor.AQUA + "Minuten");
+                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP]" + ChatColor.AQUA + " Die Hungergames starten in" + ChatColor.RED + " 5" + ChatColor.AQUA + " Minuten");
                         break;
                     case 60:
-                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP] " + ChatColor.AQUA + "Die Hungergames starten in " + ChatColor.RED + "60 " + ChatColor.AQUA + "Sekunden");
+                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP]" + ChatColor.AQUA + " Die Hungergames starten in" + ChatColor.RED + " 60" + ChatColor.AQUA + " Sekunden");
                         break;
                     case 30:
-                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP] " + ChatColor.AQUA + "Die Hungergames starten in " + ChatColor.RED + "30 " + ChatColor.AQUA + "Sekunden");
+                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP]" + ChatColor.AQUA + " Die Hungergames starten in" + ChatColor.RED + " 30" + ChatColor.AQUA + " Sekunden");
                         break;
                     case 10:
-                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP] " + ChatColor.AQUA + "Die Hungergames starten in " + ChatColor.RED + "10 " + ChatColor.AQUA + "Sekunden");
+                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP]" + ChatColor.AQUA + " Die Hungergames starten in" + ChatColor.RED + " 10" + ChatColor.AQUA + " Sekunden");
                         break;
                     case 9:
-                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP] " + ChatColor.AQUA + "Die Hungergames starten in " + ChatColor.RED + "9 " + ChatColor.AQUA + "Sekunden");
+                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP]" + ChatColor.AQUA + " Die Hungergames starten in" + ChatColor.RED + " 9" + ChatColor.AQUA + " Sekunden");
                         break;
                     case 8:
-                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP] " + ChatColor.AQUA + "Die Hungergames starten in " + ChatColor.RED + "8 " + ChatColor.AQUA + "Sekunden");
+                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP]" + ChatColor.AQUA + " Die Hungergames starten in" + ChatColor.RED + " 8" + ChatColor.AQUA + " Sekunden");
                         break;
                     case 7:
-                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP] " + ChatColor.AQUA + "Die Hungergames starten in " + ChatColor.RED + "7 " + ChatColor.AQUA + "Sekunden");
+                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP]" + ChatColor.AQUA + " Die Hungergames starten in" + ChatColor.RED + " 7" + ChatColor.AQUA + " Sekunden");
                         break;
                     case 6:
-                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP] " + ChatColor.AQUA + "Die Hungergames starten in " + ChatColor.RED + "6 " + ChatColor.AQUA + "Sekunden");
+                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP]" + ChatColor.AQUA + " Die Hungergames starten in" + ChatColor.RED + " 6" + ChatColor.AQUA + " Sekunden");
                         break;
                     case 5:
-                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP] " + ChatColor.AQUA + "Die Hungergames starten in " + ChatColor.RED + "5 " + ChatColor.AQUA + "Sekunden");
+                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP]" + ChatColor.AQUA + " Die Hungergames starten in" + ChatColor.RED + " 5" + ChatColor.AQUA + " Sekunden");
                         break;
                     case 4:
-                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP] " + ChatColor.AQUA + "Die Hungergames starten in " + ChatColor.RED + "4 " + ChatColor.AQUA + "Sekunden");
+                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP]" + ChatColor.AQUA + " Die Hungergames starten in" + ChatColor.RED + " 4" + ChatColor.AQUA + " Sekunden");
                         break;
                     case 3:
-                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP] " + ChatColor.AQUA + "Die Hungergames starten in " + ChatColor.RED + "3 " + ChatColor.AQUA + "Sekunden");
+                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP]" + ChatColor.AQUA + " Die Hungergames starten in" + ChatColor.RED + " 3" + ChatColor.AQUA + " Sekunden");
                         break;
                     case 2:
-                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP] " + ChatColor.AQUA + "Die Hungergames starten in " + ChatColor.RED + "2 " + ChatColor.AQUA + "Sekunden");
+                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP]" + ChatColor.AQUA + " Die Hungergames starten in" + ChatColor.RED + " 2" + ChatColor.AQUA + " Sekunden");
                         break;
                     case 1:
-                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP] " + ChatColor.AQUA + "Die Hungergames starten in " + ChatColor.RED + "1 " + ChatColor.AQUA + "Sekunde");
+                        Bukkit.broadcastMessage(ChatColor.GREEN + "[PMCP]" + ChatColor.AQUA + " Die Hungergames starten in" + ChatColor.RED + " 1" + ChatColor.AQUA + " Sekunde");
                         break;
                 }
                 //Ende vom Timer

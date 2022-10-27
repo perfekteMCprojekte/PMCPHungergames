8e01722b3337394a83291450ae7c02a97105e268
Iberuser <biplinfo06@gmail.com>
2022-10-27 17:18:22 +0200

    -/timer <argument> implementiert (idiotensicher) -settimer gelöscht da nicht mehr benötigt -autovervollständigung in arbeit -befehlsausgabe des timers wird noch formatiert

--- src/main/java/de/pmcp/hungergames/main.java
+++ src/main/java/de/pmcp/hungergames/main.java
@@ -2,7 +2,7 @@ package de.pmcp.hungergames;
 
 import de.pmcp.hungergames.timer.BaseTimer;
 import de.pmcp.hungergames.pregame.Freeze;
-import de.pmcp.hungergames.timer.StartTimer;
+import de.pmcp.hungergames.timer.LaunchTimer;
 import de.pmcp.hungergames.pregame.isfreeze;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
@@ -11,15 +11,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.Objects;
 
 public final class main extends JavaPlugin {
-    public static main plugin;
+    public static main plugin; //Plugin als Variable
     @Override
-    public void onEnable() {
+    public void onEnable() {  // Plugin startup logic
         plugin = this;
-        // Plugin startup logic
-        Bukkit.getLogger().info("PMCP Hungergames ist nun gestartet.");
-        //cmd register
+        Bukkit.getLogger().info("PMCP Hungergames Plugin ist nun gestartet.");
+
+        //Befehle Registrieren
         Objects.requireNonNull(this.getCommand("isfreeze")).setExecutor(new isfreeze());
-        Objects.requireNonNull(this.getCommand("starttimer")).setExecutor(new StartTimer());
+        Objects.requireNonNull(this.getCommand("launchtimer")).setExecutor(new LaunchTimer());
+        //Objects.requireNonNull(this.getCommand("basetimer")).setExecutor(new BaseTimer());
+
+        //Wird ersetzt
         Objects.requireNonNull(this.getCommand("PostTimer")).setExecutor(new BaseTimer());
 
         //listener register
@@ -28,7 +31,7 @@ public final class main extends JavaPlugin {
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public void onDisable() { // Plugin shutdown logic
+
     }
 }
---
+++ src/main/java/de/pmcp/hungergames/timer/TabCompletion.java
@@ -0,0 +1,18 @@
+package de.pmcp.hungergames.timer;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+//Autovervollständigung für Timerbefehle
+public class TabCompletion implements TabCompleter {
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return null;
+    }
+}

801456a2fb69c01b998e59d58f78316067c69b6e
Iberuser <biplinfo06@gmail.com>
2022-10-28 14:39:56 +0200

    - /...timer autovervollständigung implementiert - basetimer funktionabel gemacht (soweit möglich) - basetimer wird automatisch gestartet wenn countdown abgelaufen

--- src/main/java/de/pmcp/hungergames/main.java
+++ src/main/java/de/pmcp/hungergames/main.java
@@ -2,8 +2,9 @@ package de.pmcp.hungergames;
 
 import de.pmcp.hungergames.timer.BaseTimer;
 import de.pmcp.hungergames.pregame.Freeze;
-import de.pmcp.hungergames.timer.LaunchTimer;
 import de.pmcp.hungergames.pregame.isfreeze;
+import de.pmcp.hungergames.timer.LaunchTimer;
+import de.pmcp.hungergames.timer.TabCompletion;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -18,12 +19,12 @@ public final class main extends JavaPlugin {
         Bukkit.getLogger().info("PMCP Hungergames Plugin ist nun gestartet.");
 
         //Befehle Registrieren
-        Objects.requireNonNull(this.getCommand("isfreeze")).setExecutor(new isfreeze());
-        Objects.requireNonNull(this.getCommand("launchtimer")).setExecutor(new LaunchTimer());
-        //Objects.requireNonNull(this.getCommand("basetimer")).setExecutor(new BaseTimer());
+        Objects.requireNonNull(getCommand("isfreeze")).setExecutor(new isfreeze());
+        Objects.requireNonNull(getCommand("launchtimer")).setExecutor(new LaunchTimer());
+        Objects.requireNonNull(getCommand("launchtimer")).setTabCompleter(new TabCompletion());
+        Objects.requireNonNull(getCommand("basetimer")).setExecutor(new BaseTimer());
+        Objects.requireNonNull(getCommand("basetimer")).setTabCompleter(new TabCompletion());
 
-        //Wird ersetzt
-        Objects.requireNonNull(this.getCommand("PostTimer")).setExecutor(new BaseTimer());
 
         //listener register
         PluginManager pluginManager = Bukkit.getPluginManager();

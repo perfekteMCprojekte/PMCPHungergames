a4959196e61921184db8c55936b29df3fe6b50fc
Iberuser <biplinfo06@gmail.com>
2022-11-01 11:27:09 +0100

    newCommand() in main HA es ist jetzt überladen

--- src/main/java/de/pmcp/hungergames/main.java
+++ src/main/java/de/pmcp/hungergames/main.java
@@ -7,6 +7,8 @@ import de.pmcp.hungergames.pregame.isfreeze;
 import de.pmcp.hungergames.timer.LaunchTimer;
 import de.pmcp.hungergames.timer.TabCompletion;
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -20,13 +22,10 @@ public final class main extends JavaPlugin {
         Bukkit.getLogger().info("PMCP Hungergames Plugin ist nun gestartet.");
 
         //Befehle Registrieren
-        Objects.requireNonNull(getCommand("isfreeze")).setExecutor(new isfreeze());
-        Objects.requireNonNull(getCommand("launchtimer")).setExecutor(new LaunchTimer());
-        Objects.requireNonNull(getCommand("launchtimer")).setTabCompleter(new TabCompletion());
-        Objects.requireNonNull(getCommand("basetimer")).setExecutor(new BaseTimer());
-        Objects.requireNonNull(getCommand("basetimer")).setTabCompleter(new TabCompletion());
-        Objects.requireNonNull(getCommand("adminmsg")).setExecutor(new Adminmsg());
-
+        newCommand("isfreeze", new isfreeze());
+        newCommand("adminmsg", new Adminmsg());
+        newCommand("launchtimer", new LaunchTimer(), new TabCompletion());
+        newCommand("basetimer", new BaseTimer(), new TabCompletion());
 
         //listener register
         PluginManager pluginManager = Bukkit.getPluginManager();
@@ -36,5 +35,13 @@ public final class main extends JavaPlugin {
     @Override
     public void onDisable() { // Plugin shutdown logic
 
+    }
+
+
+    //Einfache Befehls Erstellung
+    private void newCommand(String command, CommandExecutor cmdfile) { Objects.requireNonNull(getCommand(command)).setExecutor(cmdfile); }
+    private void newCommand(String command, CommandExecutor cmdfile, TabCompleter tabfile) {
+        Objects.requireNonNull(getCommand(command)).setExecutor(cmdfile);
+        Objects.requireNonNull(getCommand(command)).setTabCompleter(tabfile);
     }
 }

49ba67169cb35c5d198607e010340f7b6aa1c192
Iberuser <116600993+Iberuser@users.noreply.github.com>
2023-02-04 20:09:41 +0100

    Update DayTimer.java

--- src/main/java/de/pmcp/hungergames/game/DayTimer.java
+++ src/main/java/de/pmcp/hungergames/game/DayTimer.java
@@ -1,6 +1,7 @@
 package de.pmcp.hungergames.game;
 
 import de.pmcp.hungergames.CMDS.Freeze;
+import de.pmcp.hungergames.CMDS.Timer;
 import de.pmcp.hungergames.game.Engine;
 import de.pmcp.hungergames.main;
 import org.apache.commons.lang.ArrayUtils;
@@ -8,6 +9,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitScheduler;
 
 public class DayTimer {
+    public static int day = Engine.day;
     int minutes = 40; //Sessiondauer Angeben
     final int[] timerPoints = {60, 30, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1}; //Benachrichtigungsstände als Abstand zum Punkt Null
     int sessionLength = minutes * 60;
@@ -16,10 +18,8 @@ public class DayTimer {
     BukkitScheduler scheduler = Bukkit.getScheduler();
 
     public void timer() {
-        int day = Engine.day;
         scheduler.runTaskTimer(main.plugin,task -> {
             if (timerPaused) return;
-
             if (time[0] < 0) {
                 int dist = time[0] * -1;
                 if (ArrayUtils.contains(timerPoints, dist))
@@ -29,12 +29,14 @@ public class DayTimer {
                         Bukkit.broadcastMessage("§e[§6Hungergames§e] §4Die Spiele starten in §c" + (dist <= -60 ? dist / 60 + " §4Minuten" : dist + " §4Sekunden"));
             }
             else if (time[0] == 0)  {
+                day += 1;
                 if (day == 0)
                     Bukkit.broadcastMessage("§6§lDie Spiele sind gestartet: \n" + "§4§lViel Glück!");
                 else
                     Bukkit.broadcastMessage("§6§lDer Tag ist gestartet: \n" + "§4§lViel Glück!");
                 Freeze.isfreeze = false;
-            }
+                Info.DailyNews();
+                }
             else if (time[0] < sessionLength) {
                 int dist = sessionLength - time[0];
                 if (ArrayUtils.contains(timerPoints, dist))

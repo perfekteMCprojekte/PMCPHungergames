790487901f1ba8899a3be2f8f640584298b47d0f
Iberuser <biplinfo06@gmail.com>
2023-02-04 20:46:44 +0100

    Aktuelle Version mit Fehlerbehebungen

--- src/main/java/de/pmcp/hungergames/game/Info.java
+++ src/main/java/de/pmcp/hungergames/game/Info.java
@@ -8,22 +8,24 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitScheduler;
 
 public class Info implements Listener {
+
     public static void info() {
-        new BukkitRunnable() {
-            @Override public void run() {
-                //Füllt die Actionbar mit "Tag" und "Todesanzahl" aus
-                for (Player player : Bukkit.getOnlinePlayers()) {
-                    if (Death.deathCount != 1) {
-                        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("§4Tag: §r§6" + Engine.day + " §r§4|| " + "§r§6" + Death.deathCount + " §r§4Leichen gefunden"));
-                    }
-                    else
-                        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("§4Tag: §6§l" + Engine.day + " §r§4|| " + "§r§6" + Death.deathCount + " §r§4Leiche gefunden"));}
+        BukkitScheduler scheduler = Bukkit.getScheduler();
+        scheduler.runTaskTimer(main.plugin, task -> { //Timer für timer
+            //Füllt die Actionbar mit "Tag" und "Todesanzahl" aus
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                if (Death.deathCount != 1) {
+                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("§4Tag: §r§6" + Engine.day + " §r§4|| " + "§r§6" + Death.deathCount + " §r§4Leichen gefunden"));
+                } else {
+                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("§4Tag: §6§l" + Engine.day + " §r§4|| " + "§r§6" + Death.deathCount + " §r§4Leiche gefunden"));
                 }
             }
-        }.runTaskTimer(main.plugin, 0, 10);
+        }, 0, 20); //Timings für Info
     }
+    //Für Tagesnachrichten
     public static void DailyNews() {
             Bukkit.broadcastMessage("\n§6§nDaily News: ");
             for (String deathMessageLoop : Death.deathArray) {

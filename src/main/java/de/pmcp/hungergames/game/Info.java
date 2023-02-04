74b5c52598c38c4762b7771f7bccde93d401db72
Iberuser <116600993+Iberuser@users.noreply.github.com>
2023-02-04 19:57:36 +0100

    Update Info.java

--- src/main/java/de/pmcp/hungergames/game/Info.java
+++ src/main/java/de/pmcp/hungergames/game/Info.java
@@ -16,8 +16,8 @@ public class Info implements Listener {
                 //Füllt die Actionbar mit "Tag" und "Todesanzahl" aus
                 for (Player player : Bukkit.getOnlinePlayers()) {
                     if (Death.deathCount != 1) {
-                        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("§4Tag: §r§6" + DayTimer.day + " §r§4|| " + "§r§6" + Death.deathCount + " §r§4Leichen gefunden"));
-                    }   else {player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("§4Tag: §r§6" + DayTimer.day + " §r§4|| " + "§r§6" + Death.deathCount + " §r§4Leiche gefunden"));}
+                        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("§4Tag: §r§6" + Engine.day + " §r§4|| " + "§r§6" + Death.deathCount + " §r§4Leichen gefunden"));
+                    }   else {player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("§4Tag: §r§6" + Engine.day + " §r§4|| " + "§r§6" + Death.deathCount + " §r§4Leiche gefunden"));}
                 }
             }
         }.runTaskTimer(main.plugin, 0, 10);

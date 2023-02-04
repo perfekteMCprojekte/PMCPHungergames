8138e8f27996f7d08b53b35c76e0b0293407c9e8
Iberuser <116600993+Iberuser@users.noreply.github.com>
2023-02-04 20:16:57 +0100

    Create Database.java

---
+++ src/main/java/de/pmcp/hungergames/game/Database.java
@@ -0,0 +1,14 @@
+package de.pmcp.hungergames.tools;
+
+public class Database {
+    //Liest daten aus dem Speicher/Config
+    public static String[] get() {
+        String[] data = {};
+
+        return data;
+    }
+    //Schreibt Daten in den Speicher/Config
+    public static void write(String[] data) {
+
+    }
+}
